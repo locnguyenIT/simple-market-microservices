eacc455e9453aab379f3c925082601889ecce906
ntloc <ntloc.developer@gmail.com>
2022-09-05 23:07:10 +0700

    change email content

--- notification/src/main/java/com/ntloc/notification/NotificationService.java
+++ notification/src/main/java/com/ntloc/notification/NotificationService.java
@@ -48,10 +48,10 @@ public class NotificationService {
     }
 
     public String buildEmail(String name, String message) {
-        return "<p>Hi " + name + ",</p>"
+        return "<p>Hi<strong> " + name + ",</strong></p>"
                 + "<p>" + message + ". Have a nice day.</p>"
                 + "<p>Best regard,</p>"
-                + "<p>PJ-AT team</p>";
+                + "<p><strong>PJ-AT team</strong></p>";
     }
 
 }
